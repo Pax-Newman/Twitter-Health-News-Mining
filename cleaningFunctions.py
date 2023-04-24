4e0fc6a730b38123c335c73191fad7b8f7d979ed
Mayla Ward <wardm22@wwu.edu>
2023-04-24 16:43:39 -0600

    updated cleaning functions

--- cleaningFunctions.py
+++ cleaningFunctions.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
 
 import pandas as pd
 import numpy as np
@@ -12,25 +16,38 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize as tokenize
 from nltk.stem.snowball import SnowballStemmer
-
 stemmer = SnowballStemmer("english")
 stemIgnStop = SnowballStemmer('english', ignore_stopwords=True)
 lemmer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidfVec = TfidfVectorizer()
 
-#load data
+
+# In[2]:
+
+
 datafile = "data/dataset.csv"
 twitter = pd.read_csv(datafile)
 
 
+# In[43]:
+
+
 #removes usernames, links, and converts to lowercase
-def remUserLinkCase(tweets):
-    return tweets.str.replace(r'http\S+','',regex=True).str.replace(r'@\S+','',regex=True).str.lower()
+def remUserLinkCase(tweets, remHash = False):
+    if remHash==True:
+        return tweets.str.replace(r'http\S+','',regex=True).str.replace(r'@\S+','',regex=True).str.replace(r'#\S+','',regex=True).str.lower()
+    else:
+        return tweets.str.replace(r'http\S+','',regex=True).str.replace(r'@\S+','',regex=True).str.lower()
 
 #removes punctuation
-def remPunct(tweets):
-    return tweets.str.replace(r'[^\w\s]','', regex=True)
+def remPunct(tweets, remNum = False):
+    if remNum==True:
+        return tweets.str.replace(r'[\d]','', regex=True).str.replace(r'[^\w\s]','', regex=True)
+    else:
+        return tweets.str.replace(r'[^\w\s]','', regex=True)
 
 #removes stop words
 def remStopWords(tweets):
@@ -61,14 +78,86 @@ def stem(tweets, remStop = True):
         return tweets
 
 
-#Examples/tests:
-'''
+# In[4]:
+
+
+cleaned = lem(remPunct(remUserLinkCase(twitter["content"])), remStop = True)
+
+
+# In[36]:
+
+
+stemmed = stem(remPunct(remUserLinkCase(twitter["content"])), remStop = True)
+
+
+# In[44]:
+
+
+noTagLem = lem(remPunct(remUserLinkCase(twitter["content"], remHash = True), remNum = False), remStop = True)
+
+
+# In[46]:
+
+
+noTagVec = tfidfVec.fit_transform(noTagLem.tolist())
+
+
+# In[51]:
+
+
+tfidfVec.get_feature_names_out().shape
+
+
+# In[17]:
+
+
+tfidf = tfidfVec.fit_transform(cleaned.tolist())
+
+
+# In[37]:
+
+
+stemmedidf = tfidfVec.fit_transform(stemmed.tolist())
+
+
+# In[41]:
+
+
+tfidfVec.get_feature_names_out()[0:1000]
+
+
+# In[31]:
+
+
+cleaned.tolist()
+
+
+# In[27]:
+
+
+cleaned[1]
+
+
+# In[ ]:
+
+
 stem(remPunct(remUserLinkCase(twitter["content"])), remStop = True)
+
+
+# In[ ]:
+
 
 stem(remPunct(remUserLinkCase(twitter["content"])), remStop = False)
 
+
+# In[ ]:
+
+
 lem(remPunct(remUserLinkCase(twitter["content"])), remStop = True)
 
+
+# In[ ]:
+
+
 lem(remPunct(remUserLinkCase(twitter["content"])), remStop = False)
-'''
 
