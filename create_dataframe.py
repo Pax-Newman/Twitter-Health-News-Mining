b2e0719061d5023edc80754e2e8a656ee4ee2c4b
Pax Newman <pax.newman@gmail.com>
2023-05-04 16:47:25 -0700

    added bert to create_dataframe script

--- create_dataframe.py
+++ create_dataframe.py
@@ -2,6 +2,8 @@ from models.embedders import FastText
 from sklearn import cluster
 from nltk.corpus import stopwords
 from re import sub
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
 import nltk
 import pandas as pd
@@ -9,9 +11,11 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str, default='data/dataset.csv')
-parser.add_argument('--save_path', type=str, default='data/dataframe')
-parser.add_argument('--n_clusters', type=int, default=6)
+parser.add_argument('--data_path', type=str, default='data/dataset.csv', help='path of the csv file to use')
+parser.add_argument('--save_path', type=str, default='data/dataframe', help='path to place the resulting dataframe')
+parser.add_argument('--n_clusters', type=int, default=6, help='how many clusters to create with kmeans')
+parser.add_argument('--embedder', type=str, default='fasttext', choices=['fasttext', 'bert'] , help='which embedder to use')
+parser.add_argument('--device', type=str, default='cpu', help='which device to use for embedding (applies only to bert)')
 
 args = parser.parse_args()
 
@@ -41,29 +45,38 @@ def clean(tweet: str) -> str:
     cleaned = ' '.join([word for word in cleaned.split(' ') if word not in stopword_set])
     return cleaned
 
-df['cleaned'] = df['content'].apply(clean)
+# Init tqdm for pandas df.apply
+tqdm.pandas()
 
-print(df.iloc[:5]['cleaned'])
-
-print('\nEmpty cleaned rows:')
-print(df[df['cleaned'] == ''])
-
-print(len(df))
-
-
-df = df.drop(df[df['cleaned'] == ''].index)
-
-df = df[df['cleaned'] != '']
-
-print(df[df['cleaned'] == ''])
-
-print(len(df))
-
-# Embed tweet content
-embedder = FastText()
-
-print('embedding content')
-df['embedding'] = df['cleaned'].apply(embedder)
+if args.embedder == 'fasttext':    
+    df['cleaned'] = df['content'].apply(clean)
+    
+    print(df.iloc[:5]['cleaned'])
+    
+    print('\nEmpty cleaned rows:')
+    print(df[df['cleaned'] == ''])
+    
+    print(len(df))
+    
+    
+    df = df.drop(df[df['cleaned'] == ''].index)
+    
+    df = df[df['cleaned'] != '']
+    
+    print(df[df['cleaned'] == ''])
+    
+    print(len(df))
+    
+    # Embed tweet content
+    embedder = FastText()
+    
+    print('embedding content')
+    df['embedding'] = df['cleaned'].progress_apply(embedder)
+elif args.embedder == 'bert':
+    embedder = SentenceTransformer('sentence-transformers/all-roberta-large-v1', device=args.device).encode
+    
+    df['embedding'] = df['content'].progress_apply(embedder)
+    # df['embedding'] = embedder.encode(tqdm(df['content']), batch_size=32)
 
 print(df.iloc[:5])
 
